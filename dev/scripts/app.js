2e0af2914e85f3780128b3802aeab0964bac675a
Vinh Luu <vinhluu16@gmail.com>
2018-01-04 21:59:53 -0500

    added create and login

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -8,6 +8,10 @@ import NoteCard from './notesCard';
 // we're going to use create user and the sign in
 //https://firebase.google.com/docs/auth/web/password-auth?authuser=0
 
+//when we do login and create user we have to go back into our database rules and change read to "auth != null" and write to "auth != null"
+//the page will now require you to be signed in to be able to see your notes
+//problem is you can create a user but can't login -- continue to step 48)
+
 //20) setting up firebase
 const config = {
   apiKey: "AIzaSyBTrOadgu8K2ZEtagP40QDLh4FuiBunBYw",
@@ -31,6 +35,10 @@ class App extends React.Component {
 
       this.showSidebar = this.showSidebar.bind(this);
       this.addNote = this.addNote.bind(this);
+      this.showCreate = this.showCreate.bind(this);
+      this.createUser = this.createUser.bind(this);
+      this.showLogin = this.showLogin.bind(this);
+      this.loginUser = this.loginUser.bind(this);
     }
 
     // 21) when we load the page we want to connect to our firebase database
@@ -131,6 +139,65 @@ class App extends React.Component {
       const dbRef = firebase.database().ref(noteKey);
       dbRef.remove();
     }
+    // 47) this.showCreate
+    // it will toggle the show class on the two things (reference the CSS)
+    showCreate(e){
+      e.preventDefault();
+      this.overlay.classList.toggle("show");
+      this.createUserModal.classList.toggle("show");
+    }
+
+    //45) setting up the createUser form event
+    // on submit we need to check that the passwords match, if so we want to create the user
+    createUser(e){
+      e.preventDefault();
+      const email = this.createEmail.value;
+      const password = this.createPassword.value; //we made a ref of this in our input **
+      const confirm = this.confirmPassword.value;
+      if(password === confirm){
+
+      // 46) now to actually create the user we just use firebase
+      //we need to send in both the email and password that we stored in variables above
+        firebase.auth()
+        //this below returns a promise so we use .then
+          .createUserWithEmailAndPassword(email, password)
+          //if the then comes back successfully we want to return something/toggle that create menu
+          //now it works and if you console.log firebase.auth().currentUser; it'll return you an object full of the users data
+          .then((res) => {
+            this.showCreate(e);
+          })
+          //catch will catch if there are any errors
+          .catch((err) => {
+            alert(err.message)
+          })
+      // 45)
+      }else{
+          alert("Passwords must match")
+        }
+      }
+
+      //50) this will toggle the login modal
+      //after all this is written, on submit of the form we want to log the user in--continue to 51
+      showLogin(e){
+        e.preventDefault();
+        this.overlay.classList.toggle("show");
+        this.loginModal.classList.toggle("show");
+      }
+
+      //51) we want to grab the email and password of the user-- continue on to 52 for allowing us to close the modal boxes
+      loginUser(){
+        const email = this.userEmail.value;
+        const password = this.userPassword.value;
+
+        firebase.auth()
+        .signInWithEmailAndPassword(email, password)
+        .then((res) => {
+          this.showLogin(e);
+        })
+        .catch((err) => {
+          alert(err.message);
+        })
+      }
 
     render() {
       return (
@@ -140,10 +207,14 @@ class App extends React.Component {
             <nav>
 
               {/* 43) we need to add a user create account 
-              - adding an onClick
               - we want a modal box to pop up*/}
-              <a href="" onClick={}></a>
-              <a href=""></a>
+
+              {/* 46) adding onClick of showCreate */}
+              <a href="" onClick={this.showCreate}>Create Account</a>
+
+              {/* 48) adding a new ubtton so we can login
+              - if you already have an account we want you to login*/}
+              <a href="" onClick={this.showLogin}>Login</a>
 
 
               {/* 1) on click here we want something to happen so we're going to add an event listener.
@@ -152,7 +223,7 @@ class App extends React.Component {
               <a href="" onClick={this.showSidebar}>Add New Note</a>
             </nav>
           </header>
-          {/* 43) */}
+          {/* 43) modal box */}
           <div className="overlay" ref={ref => this.overlay = ref}></div>
 
 {/* this is where all of our notes will live */}
@@ -199,6 +270,60 @@ If we console.log(this) in showSidebar we have all the data and the sidebar in t
               <input type="submit" value="Add New Note" />
             </form>
           </aside>
+
+          {/* 44) added all of this straight from Ryan's video 
+          - we have a div that will be our createUserModal and we have a reference to it and it has a close button
+          - we have a form that has email, password and confirm password and then a submit button
+          - */}
+
+          {/* 49) this is the login modal 
+          - have login modal with a reference to it
+          - similar to the other modal*/}
+          <div className="loginModal modal" ref={ref => this.loginModal = ref}>
+            {/* 52) just added onClick of showLogin-- has a toggle on it so it'll close*/}
+            <div className="close" onClick={this.showLogin}>
+              <i className="fa fa-times"></i>
+            </div>
+            <form action="" onSubmit={this.loginUser}>
+              <div>
+                <label htmlFor="email">Email:</label>
+                <input type="text" name="email" ref={ref => this.userEmail = ref}/>
+              </div>
+              <div>
+                <label htmlFor="password">Password:</label>
+                <input type="password" name="password" ref={ref => this.userPassword = ref} />
+              </div>
+              <div>
+                <input type="submit" value="Login"/>
+              </div>
+            </form>
+          </div>
+
+
+
+          <div className="createUserModal modal" ref={ref => this.createUserModal = ref}>
+          {/* 52) just added onClick of showCreate-- has a toggle on it so it'll close */}
+            <div className="close" onClick={this.showCreate}>
+              <i className="fa fa-times"></i>
+            </div>
+            <form action="" onSubmit={this.createUser}>
+              <div>
+                <label htmlFor="createEmail">Email:</label>
+                <input type="text" name="createEmail" ref={ref => this.createEmail = ref}/>
+              </div>
+              <div>
+                <label htmlFor="createPassword">Password:</label>
+                <input type="password" name="createPassword" ref={ref => this.createPassword = ref} />
+              </div>
+              <div>
+                <label htmlFor="confirmPassword">Confirm Password:</label>
+                <input type="password" name="confirmPassword" ref={ref => this.confirmPassword = ref} />
+              </div>
+              <div>
+                <input type="submit" value="Create"/>
+              </div>
+            </form>
+          </div>
         </div>
       )
     }
